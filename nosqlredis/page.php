4553c670d811cad908ae74073ccf2d39e0d22074
Abdel-Benamara <abdelb.contact@gmail.com>
2021-05-28 12:48:15 +0200

    OK

--- nosqlredis/page.php
+++ nosqlredis/page.php
@@ -1,119 +1,128 @@
 <?php
-    session_start();
-   //set predis
-    require "predis/autoload.php";
-    Predis\Autoloader::register();
-    try {
-        $redis = new Predis\Client(array(
-            "scheme" => "tcp",
-            "host" => "localhost",//changer le nom de la base
-            "port" => 6379
-        ));
-    }
-    catch (Exception $e) {
-        die($e->getMessage());
-    }
+session_start();
+//set predis
+require "predis/autoload.php";
+Predis\Autoloader::register();
+try {
+    $redis = new Predis\Client(array(
+        "scheme" => "tcp",
+        "host" => "localhost",//changer le nom de la base
+        "port" => 6379
+    ));
+} catch (Exception $e) {
+    die($e->getMessage());
+}
 
-    //player informations
-    if(isset($_POST["userName"])){
-        $_SESSION["userName"] = $_POST["userName"];
-        $redis->sadd("username", $_SESSION["userName"]);
-        $redis->set($_POST["userName"],"0");
-    }
+//player informations
+if (isset($_POST["userName"])) {
+    $_SESSION["userName"] = $_POST["userName"];
+    $redis->sadd("username", $_SESSION["userName"]);
+    $redis->set($_POST["userName"], "0");
+}
 
-    if(isset($_POST["wordToPlay"])){
-        if(!$redis->exists("wordToPlay")){
+if (isset($_POST["wordToPlay"])) {
+    if (!$redis->exists("wordToPlay")) {
 
-            $redis->setex("wordToPlay", 60,  $_POST["wordToPlay"]);
-            if($redis->exists("propositions")){
-                $redis->del("propositions");
-            }
-
-            if($redis->exists("nbError")){
-                $redis->del("nbError");
-            }
-            $redis->set("nbError", 0);
+        $redis->setnx("wordToPlay", $_POST["wordToPlay"]);
+        $redis->expire("wordToPlay", 60);
+        if ($redis->exists("propositions")) {
+            $redis->del("propositions");
         }
+
+        if ($redis->exists("nbError")) {
+            $redis->del("nbError");
+        }
+        $redis->set("nbError", 0);
     }
+}
 
-    if($redis->exists("wordToPlay")){
-        $isComplet = false;
+if ($redis->exists("wordToPlay")) {
+    $isComplet = false;
 
-        if(isset($_POST["letterPropal"])){
-            if(!$redis->sismember("propositions", $_POST["letterPropal"])){
-                $redis->sadd("propositions", $_POST["letterPropal"]);
-            }
+    if (isset($_POST["letterPropal"])) {
+        if (!$redis->sismember("propositions", $_POST["letterPropal"])) {
+            $redis->sadd("propositions", $_POST["letterPropal"]);
 
             $letterInWord = false;
-            foreach (str_split($redis->get("wordToPlay"),1) as $c) {
+            foreach (str_split($redis->get("wordToPlay"), 1) as $c) {
                 if ($_POST["letterPropal"] == $c) {
                     $letterInWord = true;
                     break;
                 }
             }
 
+            if (!$letterInWord) {
+                $redis->incr("nbError");
+            }
+
             //as-t-il gagné ?
-            foreach (str_split($redis->get("wordToPlay"),1) as $c){
-                if($redis->sismember("propositions", $c)){
+            foreach (str_split($redis->get("wordToPlay"), 1) as $c) {
+                if ($redis->sismember("propositions", $c)) {
                     $isComplet = true;
-                }else{
+                } else {
                     $isComplet = false;
                     break;
                 }
             }
         }
+    }
 
-        if(isset($_POST["wordPropal"])){
-            if(!$redis->sismember("propositions", $_POST["wordPropal"])){
-                $redis->sadd("propositions", $_POST["wordPropal"]);
-            }
+    if (isset($_POST["wordPropal"])) {
+        if (!$redis->sismember("propositions", $_POST["wordPropal"])) {
+            $redis->sadd("propositions", $_POST["wordPropal"]);
 
             //Si mot gagné
-            if($_POST["wordPropal"] == $redis->get("wordToPlay")){
+            if ($_POST["wordPropal"] == $redis->get("wordToPlay")) {
                 $isComplet = true;
-            }else{
+            } else {
                 $redis->incr("nbError");
             }
         }
+    }
 
-        if($isComplet){
-            //gagné
+    if ($isComplet) {
+        //gagné
+        $redis->incr($_SESSION["userName"]);
 
-            $redis->incr($_SESSION["userName"]);
+        if ($redis->exists("propositions")) {
+            $redis->del("propositions");
+        }
 
-            if($redis->exists("propositions")){
-                $redis->del("propositions");
-            }
+        if ($redis->exists("nbError")) {
+            $redis->del("nbError");
+        }
 
-            if($redis->exists("nbError")){
-                $redis->del("nbError");
-            }
+        $redis->del("wordToPlay");
+    }
 
-            $redis->del("wordToPlay");
+    if ($redis->get("nbError") >= 10) {
+        $redis->del("wordToPlay");
+    }
+}
+
+
+function getPartialWord($redis)
+{
+    $word = $redis->get("wordToPlay");
+    $show = "";
+
+    foreach (str_split($word, 1) as $c) {
+        if ($redis->sismember("propositions", $c)) {
+            $show .= $c . " ";
+        } else {
+            $show .= "_ ";
         }
     }
+    return $show;
+}
 
+$players = $redis->smembers("username");
 
-    function getPartialWord($redis) {
-        $word = $redis->get("wordToPlay");
-        $show = "";
-
-        foreach (str_split($word, 1) as $c){
-            if($redis->sismember("propositions", $c)){
-                $show .= $c." ";
-            }else{
-                $show .= "_ ";
-            }
-        }
-        return $show;
-    }
-    $players =  $redis->smembers("username");
-
-    if($redis->exists("propositions")){
-        $propal = $redis->smembers("propositions");
-    }else{
-        $propal = array();
-    }
+if ($redis->exists("propositions")) {
+    $propal = $redis->smembers("propositions");
+} else {
+    $propal = array();
+}
 ?>
 
 <!doctype html>
@@ -124,14 +133,15 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Le PeNdU</title>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
+          integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
 </head>
 <body>
 
 <nav class="navbar navbar-light bg-light">
     <span class="navbar-brand mb-0 h1">Le PeNdU</span>
     <span class="navbar-text">
-      Bonjour <?php echo $_SESSION["userName"];?>, ton score est <?php echo $redis->get($_SESSION["userName"]); ?> points !
+      Bonjour <?php echo $_SESSION["userName"]; ?>, ton score est <?php echo $redis->get($_SESSION["userName"]); ?> points !
     </span>
 </nav>
 <div class="row">
@@ -139,64 +149,86 @@
         <h2>Liste des joueurs</h2>
         <ul>
             <?php
-            foreach ($players as $p){
+            foreach ($players as $p) {
                 echo "<li>$p</li>";
             }
             ?>
         </ul>
     </div>
 
-<div class="container">
-    <?php if ($redis->exists("wordToPlay")) { ?>
+    <div class="container">
+        <?php if ($redis->exists("wordToPlay")) { ?>
 
-            <div class="col-sm-6">
-                <h2>Mot à trouver</h2>
-                <span><?php echo getPartialWord($redis)?></span>
+            <div class="row">
+                <div class="col-sm-6">
+                    <h2>Mot à trouver</h2>
+                    <span><?php echo getPartialWord($redis) ?></span>
+                </div>
+                <div class="col-sm-3">
+                    <h2>Propositions</h2>
+                    <ul>
+                        <?php foreach ($propal as $p) {
+                            echo "<li>$p</li>";
+                        } ?>
+                    </ul>
+                </div>
+                <div class="col-sm-6">
+                    <h2>Temps restant</h2>
+                    <span><span id="ttl-word"><?php echo $redis->ttl("wordToPlay") ?></span> secondes</span>
+                </div>
+                <div class="col-sm-6">
+                    <h2>Nombre d'essais restant</h2>
+                    <span><?php echo 10 - $redis->get("nbError") ?> essais</span>
+                </div>
             </div>
 
-            <div class="col-sm-3">
-                <h2>Propositions</h2>
-                <ul>
-                    <?php
-                    foreach ($propal as $p){
-                        echo "<li>$p</li>";
-                    }
-                    ?>
-                </ul>
+            <div class="row">
+                <div class="col-sm-6">
+                    <h2>Proposer une lettre</h2>
+                    <form action="page.php" method="post">
+                        <span>
+                            <input type="text" min="1" max="1" size="3" name="letterPropal" required/>
+                            <button type="submit">Valider</button>
+                        </span>
+                    </form>
+                </div>
+                <div class="col-sm-6">
+                    <h2>Proposer un mot</h2>
+                    <form action="page.php" method="post">
+                        <span>
+                            <input type="text" min="1" size="20" name="wordPropal" required/>
+                            <button type="submit">Valider</button>
+                        </span>
+                    </form>
+                </div>
             </div>
-        </div>
-        <div class="row">
-            <div class="col-sm-6">
-                <h2>Temps restant</h2>
-                <span>45 secondes</span>
-            </div>
-            <div class="col-sm-6">
-                <h2>Nombre d'essais restant</h2>
-                <span>3 essais</span>
-            </div>
-        </div>
-        <div class="row">
-            <div class="col-sm-6">
-                <h2>Proposer une lettre</h2>
-                <form action="page.php" method="post"><span><input type="text" size="3" name="letterPropal"/><button type="submit">Valider</button></span></form>
-            </div>
-            <div class="col-sm-6">
-                <h2>Proposer un mot</h2>
-                <form action="page.php" method="post"><span><input type="text" size="20" name="wordPropal"/><button type="submit">Valider</button></span></form>
-            </div>
-        </div>
 
 
-    <?php }else{ ?>
+        <?php } else { ?>
 
-        <div class="row">
-            <div class="col-sm-6">
-                <h2>Proposer un mot </h2>
-                <form action="page.php" method="post"><span><input type="text" size="30" name="wordToPlay"/><button type="submit">Valider</button></span></form>
+            <div class="row">
+                <div class="col-sm-6">
+                    <h2>Proposer un mot </h2>
+                    <form action="page.php" method="post">
+                        <span>
+                            <input type="text" size="30" name="wordToPlay"/>
+                            <button type="submit">Valider</button>
+                        </span>
+                    </form>
+                </div>
             </div>
-        </div>
 
-    <?php } ?>
+        <?php } ?>
+    </div>
 </div>
+<script>
+    setInterval(() => {
+        let timeMax = parseInt(document.getElementById("ttl-word").innerText, 10);
+        document.getElementById("ttl-word").innerText = (--timeMax).toString();
+        if (timeMax === 0) {
+            location.reload();
+        }
+    }, 1000);
+</script>
 </body>
 </html>
